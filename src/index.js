24ff9908d453bc6c7b7cc54f374623840d9b5521
andretheprogrammer <andre.lundstrom@gmail.com>
2021-10-10 17:32:13 +0200

    api folder added

--- src/index.js
+++ src/index.js
@@ -1,39 +1,15 @@
 import "./index.scss";
+import { products } from "./api";
+let { loadProduct, loadProducts } = products;
 
-let request = new XMLHttpRequest();
-let requestUrl = "./data/products.json";
-request.open("GET", requestUrl);
-request.responseType = "json";
-request.send();
+let onLoad = (ev) => {
+  let { response, status } = ev.target;
+  if (status !== 200) throw new Error("Bad result");
 
-let jsonRequest = new XMLHttpRequest();
-let jsonRequestURL1 = "./data/1.json";
-let jsonRequestURL2 = "./data/2.json";
-let jsonRequestURL3 = "./data/3.json";
-let jsonRequestURL4 = "./data/4.json";
-let jsonRequestURL5 = "./data/5.json";
-let jsonRequestURL6 = "./data/6.json";
-let jsonRequestURL7 = "./data/7.json";
-let jsonRequestURL8 = "./data/8.json";
-
-
-jsonRequest.onload = function () {
-  const rawFile1 = jsonRequest.response;
-  if (rawFile1) {
-    showModalProducts(rawFile1);
-  } else {
-    alert("no json found");
-  }
+  showProducts(response);
 };
 
-request.onload = function () {
-  const rawFile = request.response;
-  if (rawFile) {
-    showProducts(rawFile);
-  } else {
-    alert("no json found");
-  }
-};
+loadProducts(onLoad);
 
 function showModalProducts(obj) {
   const modalProduct = obj;
@@ -57,25 +33,16 @@ function showModalProducts(obj) {
 function showProducts(obj) {
   const products = obj;
 
-
-
   for (let i = 0; i < products.length; i++) {
     const productItem = document.createElement("section");
-    const productTitle = document.createElement("h3");
+    const productTitle = document.createElement("p");
+    productTitle.classList.add("product-title");
     const productPrice = document.createElement("p");
     const productCurrency = document.createElement("p");
     const myList = document.createElement("ul");
 
     productTitle.textContent = products[i].name;
     productPrice.textContent = products[i].price + " " + products[i].currency;
-
-    // const productNames = products[i].name;
-    // for (let j = 0; j < productNames.length; j++) {
-    //   const listItem = document.createElement("li");
-    //   listItem.name = productNames[j];
-    //   myList.appendChild(listItem);
-    // }
-    // const productImages = products[i].thumbnail;
 
     productItem.appendChild(myList);
     productItem.appendChild(productTitle).querySelector(".productTitle");
@@ -84,33 +51,9 @@ function showProducts(obj) {
     let imgDOM = document.createElement("img");
     imgDOM.src = products[i].thumbnail;
     // Clicked Product
-    imgDOM.onclick = function (  ) {
+    imgDOM.onclick = function () {
       modal.style.display = "block";
-      let Url = 0;
-      if (products[i].id == 1) {
-        Url = jsonRequestURL1;
-      }
-      if (products[i].id == 2) {
-        Url = jsonRequestURL2;
-      }
-      if (products[i].id == 3) {
-        Url = jsonRequestURL3;
-      }
-      if (products[i].id == 4) {
-        Url = jsonRequestURL4;
-      }
-      if (products[i].id == 5) {
-        Url = jsonRequestURL5;
-      }
-      if (products[i].id == 6) {
-        Url = jsonRequestURL6;
-      }
-      if (products[i].id == 7) {
-        Url = jsonRequestURL7;
-      }
-      if (products[i].id == 8) {
-        Url = jsonRequestURL8;
-      }
+
       let productModalName = document.getElementById("modal-name");
       let productModalImg = document.getElementById("modal-img");
       let productModalDesc = document.getElementById("modal-desc");
@@ -118,18 +61,15 @@ function showProducts(obj) {
       let productModalCurrency = document.getElementById("modal-currency");
       let activateBuyButton = document.getElementById("buy-btn");
 
+      loadProduct(i).then((res) => {
+        (productModalDesc.textContent = res.description),
+          (productModalCurrency.textContent = res.currency);
+      });
 
-      fetch(Url)
-        .then(data => {return data.json()})
-        .then(res => { productModalDesc.textContent = res.description, productModalCurrency.textContent = res.currency;})
-        .catch(err => console.log(err))
-
-        productModalName.textContent = products[i].name;
-        productModalImg.src = products[i].thumbnail;
-        productModalPrice.textContent = "Price: " + products[i].price;
-        activateBuyButton.style.display = "block";
-
-        // productModalDesc.textContent = selectedProduct.description;
+      productModalName.textContent = products[i].name;
+      productModalImg.src = products[i].thumbnail;
+      productModalPrice.textContent = "Price: " + products[i].price;
+      activateBuyButton.style.display = "block";
     };
     myList.appendChild(imgDOM);
 
@@ -147,16 +87,15 @@ aboutModalBtn.onclick = function () {
   let aboutCurrency = document.getElementById("modal-currency");
   let disableBuyButton = document.getElementById("buy-btn");
 
-  
   aboutTitle.textContent = "About Panagora";
-  aboutImg.src = "https://images.ctfassets.net/fqa5nayer5tz/1fAN9sUD1T4N8o6clNRbD1/4113fab7def6b9da1d8c924dfe2d91d6/Panagora-office-5.jpg?fm=jpg&q=75&w=2560";
-  aboutDesc.textContent = "We always bring passion, curiosity and solid teamwork to the table. All of this, coupled with our expertise in our respective fields, ensures we deliver truly unique solutions. We pride ourselves in having diverse skills, cultural backgrounds and personalities. We make sure that community and individual development is at the heart of everything that we do.  We are Panagora.";
+  aboutImg.src =
+    "https://images.ctfassets.net/fqa5nayer5tz/1fAN9sUD1T4N8o6clNRbD1/4113fab7def6b9da1d8c924dfe2d91d6/Panagora-office-5.jpg?fm=jpg&q=75&w=2560";
+  aboutDesc.textContent =
+    "We always bring passion, curiosity and solid teamwork to the table. All of this, coupled with our expertise in our respective fields, ensures we deliver truly unique solutions. We pride ourselves in having diverse skills, cultural backgrounds and personalities. We make sure that community and individual development is at the heart of everything that we do.  We are Panagora.";
   aboutPrice.textContent = "";
   aboutCurrency.textContent = "";
   disableBuyButton.style.display = "none";
-}
-
-
+};
 
 // Get the modal
 var modal = document.getElementById("myModal");
